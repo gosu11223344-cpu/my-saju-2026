6df9ebd992df431ab94d8dfde9822ffea03582c9
gosu11223344-cpu <gosu11223344@gmail.com>
2026-01-20 00:20:33 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "24px", fontFamily: "system-ui, -apple-system, sans-serif" }}>
+      <h1>사주 리포트</h1>
+      <p>사이트가 정상적으로 실행되었습니다.</p>
+    </div>
+  );
+}
+
+export default App;
