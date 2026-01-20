2c65108de70f2e853df0388d79ed42756a6f8e60
김기태 <gosu11223344@gmail.com>
2026-01-21 01:46:11 +0900

    fix: tailwind css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> 52b69dc (init: my-saju-2026 project)
